8b51137722ba70a0493a0ee2712a454cad8dd6a4
Grepleon <kharit.artem@gmail.com>
2025-11-02 19:42:09 +0300

    Водные существа
    Были добавлены амфибии и леденящий был добавлен лед

--- The ruined City.py
+++ The ruined City.py
@@ -49,28 +49,80 @@
 
             if d <= 1:
                 hp -= 1 # для босса нормально, тестил
+        if e.T == 'Амфибия':
+            dx = min(abs(e.x - Nx), X - abs(e.x - Nx))
+            dy = min(abs(e.y - Ny), Y - abs(e.y - Ny))
+            d = (dx ** 2 + dy ** 2) ** 0.5
+
+            if d <= 1:
+                hp -= 0.2
     else f"УБЕЙ БОССА!\n{HP_BOSS}/1000 хп")
+                if T == 'Лед':
+                    canvas.itemconfig(S, fill='#6060aa', outline='#eaeaea')
                             if n.T == 'Мега-Рыцарь': # босс
                                 canvas.itemconfig(S, fill='#440011', outline='#ffaaaa')
                             if n.T == 'Мега-Рыцарь': # босс
                                 canvas.itemconfig(S, fill='#110000', outline='#440011')# ты как бы чувствуешь из далека босса, сделано для баланса
+                            if n.T == 'Амфибия':
+                                canvas.itemconfig(S, fill='#000080', outline='#4169e1')
+                            if n.T == 'Леденящий':
+                                canvas.itemconfig(S, fill='#0080e0', outline='#4169e1')
+                elif T == 'Лед':
+                    canvas.itemconfig(S, fill='#000000', outline='#112266')
                 if [x, y] in Ec: # плохиш
                     for n in E:
                         if [x, y] == [n.x, n.y]:
                             if n.T == 'Мега-Рыцарь': # босс
                                 canvas.itemconfig(S, fill='#000000', outline='#000000')# ты как бы чувствуешь из далека босса, сделано для баланса
+                        if T in ['Вода']:
+                            if Game in [-7]:
+                                E.append(Enemy("Амфибия", x, y, hp=225))
+                                Ec.append([x, y])
+                            if Game in [-8]:
+                                E.append(Enemy("Леденящий", x, y, hp=225))
+                                Ec.append([x, y])
                 if random.randint(1, X * Y * 25) == 1:
                     if not ([x, y] in Ec):
                         if T in ['Земля']:
                             if Game in [BOSS_LEVEL]:  # ключ для нанесения урона боссу (по другому нельзя)
                                 E.append(Enemy('Ключ', x, y))
                                 Ec.append([x, y])
+                if T == 'Лед':
+                    hp -= 0.2
                 if e.T == 'Ключ':
                     if [Nx, Ny] == [e.x, e.y]:
                         E.remove(e)
                         Ec.remove([e.x, e.y])
                         KEY_X = Nx
                         KEY_Y = Ny
+                if e.T == 'Леденящий':
+                    if IF(e.x, e.y, x, y):
+                        Temp[i] -= 5
+                    if [e.x, e.y] == [x, y]:
+                        if Temp[i] >= 25:
+                            if Game in [-8]:
+                                KILL_KNIGHT += 1
+                            E.remove(e)
+                            Ec.remove([e.x, e.y])
+                if e.T == 'Амфибия':
+                    if Type[e.x * Y + e.y] != 'Вода':
+                        if N % 2 == 0:
+                            if e.Hod:
+                                x2, y2 = e.x, e.y
+                                e.Move_Everywhere(Nx, Ny)
+                                Ec[Ec.index([x2, y2])] = [e.x, e.y]
+                                e.Hod = not e.Hod
+                    else:
+                        if e.Hod:
+                            x2, y2 = e.x, e.y
+                            e.Move_Everywhere(Nx, Ny)
+                            Ec[Ec.index([x2, y2])] = [e.x, e.y]
+                            e.Hod = not e.Hod
+                    if TO_ATTACK_SWORD is not None and (e.x, e.y) in TO_ATTACK_SWORD and ATTACK:
+                        if e in E:
+                            E.remove(e)
+                        if [e.x, e.y] in Ec:
+                            Ec.remove([e.x, e.y])
                 if e.T == 'Мега-Рыцарь':
                     if IF(e.x, e.y, x, y):
                         if T == 'Вода': # Босс 'затопчивает' воду
@@ -89,6 +141,18 @@
                         e.y = Y_BOSS + 1
                     Ec[int(e.t) - 1][0] = e.x
                     Ec[int(e.t) - 1][1] = e.y
+            if Game <= -8:
+                if Temp[i] < -45:
+                    if T == 'Лед':
+                        Temp[i] = -45
+
+                if Temp[i] < -25:
+                    if T == 'Вода':
+                        Type[i] = 'Лед'
+
+                if Temp[i] > -25:
+                    if T == 'Лед':
+                        Type[i] = 'Вода'
     for x in range(X):
         for y in range(Y):
             if (x == KEY_X or y == KEY_Y) and (x != KEY_X or y != KEY_Y):
